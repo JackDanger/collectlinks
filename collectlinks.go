910f1d9a3ff7841630885751e1bcfe3f18a7a40e
Jack Danger Canty <jackdanger@squareup.com>
2014-05-09 08:17:23 -0700

    creating a way to simplify html parsing

---
+++ collectlinks.go
@@ -0,0 +1,33 @@
+// This package does the extraordinarily simple operation of parsing a given piece of html
+// and providing you with all the hyperlinks hrefs it finds.
+package collectlinks
+
+import (
+  "code.google.com/p/go.net/html"
+  "io"
+)
+
+/* `All` takes a reader object (like the one returned from http.Get())
+   It returns a slice of strings representing the "href" attributes from
+   anchor links found in the provided html.
+
+   It does not close the reader passed to it.
+*/
+func All(httpBody io.Reader) []string {
+  links := make([]string, 0)
+  page := html.NewTokenizer(httpBody)
+  for { 
+    tokenType := page.Next() 
+    if tokenType == html.ErrorToken {
+      return links
+    }       
+    token := page.Token()
+    if tokenType == html.StartTagToken && token.DataAtom.String() == "a" {
+      for _, attr := range token.Attr {
+        if attr.Key == "href" {
+          links = append(links, attr.Val)
+        }
+      }
+    }
+  }
+}
---
+++ collectlinks_test.go
@@ -0,0 +1,34 @@
+package collectlinks
+
+import (
+  "fmt"
+  "testing"
+  "strings"
+)
+
+func TestAll(t *testing.T) {
+  reader := strings.NewReader(` <p>
+  <a href="http://motherfuckingwebsite.com">1</a>
+  <a href='http://news.google.com'>2</a>
+  <a style=\"\" href=http://imgur.com>3</a>
+  http://dontcollectthis.com
+</p>`)
+
+  links := All(reader)
+
+fmt.Println(links)
+
+  if len(links) != 3 {
+    t.Error("Wrong number of links returned")
+  }
+
+  if links[0] != "http://motherfuckingwebsite.com" {
+    t.Error("The first link is incorrect")
+  }
+  if links[1] != "http://news.google.com" {
+    t.Error("The second link is incorrect")
+  }
+  if links[2] != "http://imgur.com" {
+    t.Error("The third link is incorrect")
+  }
+}
