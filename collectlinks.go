d9fc24fca50a71d92f068bbfccefbb0f263cf097
Dan Croak <dan@thoughtbot.com>
2014-05-11 10:28:41 -0700

    Apply go fmt to collectlinks.go

--- collectlinks.go
+++ collectlinks.go
@@ -3,8 +3,8 @@
 package collectlinks
 
 import (
-  "code.google.com/p/go.net/html"
-  "io"
+	"code.google.com/p/go.net/html"
+	"io"
 )
 
 /* All takes a reader object (like the one returned from http.Get())
@@ -14,20 +14,20 @@ import (
    It does not close the reader passed to it.
 */
 func All(httpBody io.Reader) []string {
-  var links []string
-  page := html.NewTokenizer(httpBody)
-  for {
-    tokenType := page.Next()
-    if tokenType == html.ErrorToken {
-      return links
-    }
-    token := page.Token()
-    if tokenType == html.StartTagToken && token.DataAtom.String() == "a" {
-      for _, attr := range token.Attr {
-        if attr.Key == "href" {
-          links = append(links, attr.Val)
-        }
-      }
-    }
-  }
+	var links []string
+	page := html.NewTokenizer(httpBody)
+	for {
+		tokenType := page.Next()
+		if tokenType == html.ErrorToken {
+			return links
+		}
+		token := page.Token()
+		if tokenType == html.StartTagToken && token.DataAtom.String() == "a" {
+			for _, attr := range token.Attr {
+				if attr.Key == "href" {
+					links = append(links, attr.Val)
+				}
+			}
+		}
+	}
 }
